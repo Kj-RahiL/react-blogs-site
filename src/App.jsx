2dd629b45a16ec43054c3ad77a8142448f35c917
Kj-RahiL <rahiilarham@gmail.com>
2023-09-14 15:12:27 +0600

    'updated'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      
-      <h1 className=' text-4xl'>Knowladge Cafe</h1>
+
       <Header></Header>
+      <div className=' md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Blog from "../Blog/blog";
+
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([])
+
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res=> res.json())
+        .then(data=> setBlogs(data))
+    },[])
+
+    return (
+        <div className=" w-2/3">
+            <h2 className=" text-2xl font-semibold">Blogs:{blogs.length}</h2>
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
+            
+        </div>
+    );
+};
+
+export default Blogs;
